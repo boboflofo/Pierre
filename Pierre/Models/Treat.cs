ea0be6bcdc262033992b60e84970f088bdc940ff
Jonathan Song <jonats10@uci.edu>
2024-02-16 10:37:24 -0800

    add model and many to many relationships

---
+++ Pierre/Models/Treat.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pierre.Models
+{
+  public class Treat
+    {
+        public int TreatId { get; set; }
+        [Required(ErrorMessage = "The treat's name can't be empty!")]
+        public string Name { get; set; }
+        public List<TreatFlavor> JoinEntities { get;}
+    }
+}
